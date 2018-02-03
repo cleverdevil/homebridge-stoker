64f84002bd16519975f8abf4e63b950d6f6a961b
Jonathan LaCour <jonathan@cleverdevil.org>
2018-02-02 23:18:24 -0800

    It lives! I had many, many issues with the initial, untested version. But, now it works.

--- index.js
+++ index.js
@@ -13,72 +13,80 @@ function Stoker(log, config) {
     this.name = config["name"];
     this.host = config["stoker_address"];
     this.pollingInterval = config["polling_interval"] || 5;
+
+    // init sensors, targets, and blowers
     this.sensors = {};
     this.targets = {};
     this.blowers = {};
-
-    var callback = function(err, value) {
+    this.init(config.sensors, config.blowers);
+    
+    var self = this;
+    var callback = function() {
         setTimeout(function() {
             self.getState(callback);
         }, self.pollingInterval * 1000);
-
-        if (err !== null)
-            return;
     };
     
-    self.init(callback);
+    this.getState(callback);
 }
 
-Stoker.prototype.init = function(callback) {
-    this.log("Initializing sensors from Stoker.");
+Stoker.prototype.init = function(sensorsData, blowersData) {
+    this.log("Initializing services from configuration.");
 
-    request.get({
-        url: "http://" + this.host + "/stoker.json"
-    }, function (err, response, body) {
-        if (err || response.statusCode !== 200) {
-            var statusCode = response ? response.statusCode : 1;
-            this.log("Error getting state (status code %s): %s", statusCode, err);
-            return false;
-        }
-
-        this.log("Got response from Stoker.");
-
-        var data = JSON.parse(body);
-
-        // loop through the temperature sensors
-        data.stoker.sensors.forEach(function(sensorData) {
-            // create a temperature sensor service for each temperature sensor
-            this.log("Creating TemperatureSensor: " + sensorData.name);
-            this.sensors[sensorData.name] = new Service.TemperatureSensor(sensorData.name);
+    // loop through the temperature sensors
+    sensorsData.forEach(function(sensorData) {
+        // create a temperature sensor service for each temperature sensor
+        this.log("Creating TemperatureSensor: " + sensorData.name);
+        this.sensors[sensorData.name] = new Service.TemperatureSensor(sensorData.name, sensorData.name);
             
-            // create a "target" occupancy sensor for when the temperature sensor reaches
-            // its target temperature
-            this.log("Creating OccupancySensor (target): " + sensorData.name);
-            this.targets[sensorData.name] = new Service.OccupancySensor(sensorData.name);
-        }.bind(this));
+        // create a "target" occupancy sensor for when the temperature sensor reaches
+        // its target temperature
+        this.log("Creating OccupancySensor (target): " + sensorData.name);
+        this.targets[sensorData.name] = new Service.OccupancySensor(sensorData.name + " Alarm", sensorData.name);
+    }.bind(this));
         
-        // loop through the blowers
-        data.stoker.blowers.forEach(function(blowerData) {
-            // create a blower occupancy sensor for when the blower is engaged
-            this.log("Creating OccupancySensor (blower): " + blowerData.name);
-            this.blowers[blowerData.name] = new Service.OccupancySensor(blowerData.name);
-        }.bind(this));
-
-        callback();
+    // loop through the blowers
+    blowersData.forEach(function(blowerData) {
+        // create a blower occupancy sensor for when the blower is engaged
+        this.log("Creating OccupancySensor (blower): " + blowerData.name);
+        this.blowers[blowerData.name] = new Service.OccupancySensor(blowerData.name, blowerData.name);
     }.bind(this));
     
+    this.log("Services initiualized with " + 
+             Object.keys(this.sensors).length + 
+             " sensor(s) and " + 
+             Object.keys(this.blowers).length +
+             " blower(s).");
+}
+
+Stoker.prototype.fahrenheitToCelsius = function(temperature) {
+    return (temperature - 32) / 1.8;
 }
 
 Stoker.prototype.getState = function(callback) {
     this.log("Getting current state...");
 
     request.get({
-        url: "http://" + this.host + "/stoker.json"
+        url: "http://" + this.host + "/stoker.json",
+        timeout: 5000
     }, function (err, response, body) {
         if (err || response.statusCode !== 200) {
             var statusCode = response ? response.statusCode : 1;
-            this.log("Error getting state (status code %s): %s", statusCode, err);
-            callback(err);
+            this.log("Error getting state. Stoker may be inactive."); 
+            
+            Object.keys(this.targets).forEach(function(targetName) {
+                this.targets[targetName]
+                    .getCharacteristic(Characteristic.OccupancyDetected)
+                    .updateValue(false);
+            }.bind(this));
+
+            Object.keys(this.blowers).forEach(function(blowerName) {
+                this.blowers[blowerName]
+                    .getCharacteristic(Characteristic.OccupancyDetected)
+                    .updateValue(false);
+            }.bind(this));
+
+            callback();
             return;
         }
         
@@ -92,13 +100,13 @@ Stoker.prototype.getState = function(callback) {
             this.log("Updating TemperatureSensor (" + sensorData.name + ") -> " + sensorData.tc);
             this.sensors[sensorData.name]
                 .getCharacteristic(Characteristic.CurrentTemperature)
-                .updateValue(sensorData.tc, null);
+                .updateValue(this.fahrenheitToCelsius(sensorData.tc), null);
             
             // update the target occupancy service to reflect the alarm state
             this.log("Updating OccupancySensor (" + sensorData.name + ") -> " + (sensorData.al != 0));
             this.targets[sensorData.name]
                 .getCharacteristic(Characteristic.OccupancyDetected)
-                .updateValue(sensorData.al != 0);
+                .updateValue(sensorData.tc >= sensorData.ta);
         }.bind(this));
         
         // loop through the blowers
@@ -110,7 +118,7 @@ Stoker.prototype.getState = function(callback) {
                 .updateValue(blowerData.on == 1);
         }.bind(this));
 
-        callback(null, true);
+        callback();
 
     }.bind(this));
 }
